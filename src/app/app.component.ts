3a3874f1834022d405e2bdb150d3ebe7176d7111
trev0ux <lucas.lopes5@hotmail.com>
2019-10-25 08:31:01 -0300

    efetuar autenticação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -10,12 +10,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  postData = {
+    "email": "tlopes@gmail.com",
+    "password": "123456"
+  };
+  
+  url = 'http://45.55.144.89/fsapi/users/login';
+  json;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private http: HttpClient
   ) {
     this.initializeApp();
+
+    this.http.post(this.url, this.postData).toPromise().then((data:any) => {
+      console.log(data);
+      console.log(data.json);
+      this.json = JSON.stringify(data.json);
+    });
   }
 
   initializeApp() {
